366fcf2be08863d66092d462bcd3ad637f3d0ebc
Roshan R <roshan@Macbook-Pro-2.local>
2023-10-03 19:41:17 +0530

    Created contacts component and added a route to it

---
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "@bigbinary/neetoui/layouts";
+
+const Contacts = () => (
+  <Container>
+    <h1>Contacts Page</h1>
+  </Container>
+);
+
+export default Contacts;
