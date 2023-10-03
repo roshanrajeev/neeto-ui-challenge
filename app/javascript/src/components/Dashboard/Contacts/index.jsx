7ee1f7023e473d3531a52e8087eaa2a99fbd820b
Roshan R <roshan@Macbook-Pro-2.local>
2023-10-03 19:57:14 +0530

    Added header to contacts page

--- app/javascript/src/components/Dashboard/Contacts/index.jsx
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
-import { Container } from "@bigbinary/neetoui/layouts";
+import { Button } from "@bigbinary/neetoui";
+import { Container, Header } from "@bigbinary/neetoui/layouts";
+import { useTranslation } from "react-i18next";
 
-const Contacts = () => (
-  <Container>
-    <h1>Contacts Page</h1>
-  </Container>
-);
+import { noop } from "components/utils";
+
+const Contacts = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <Header
+        title={t("titles.contacts")}
+        actionBlock={
+          <Button
+            icon="ri-add-line"
+            label={t("buttons.add_entity", { entity: "Contacts" })}
+            size="small"
+            onClick={noop}
+          />
+        }
+        searchProps={{
+          value: "",
+          onChange: noop,
+          unlimitedChars: false,
+        }}
+      />
+    </Container>
+  );
+};
 
 export default Contacts;
