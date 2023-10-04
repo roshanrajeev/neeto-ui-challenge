a51fec2961faddf4734caed6da089cec24f75238
Roshan R <roshan@Macbook-Pro-2.local>
2023-10-04 08:28:48 +0530

    Fixed format

--- app/javascript/src/components/Dashboard/Notes/utils.js
+++ app/javascript/src/components/Dashboard/Notes/utils.js
@@ -1,15 +1,12 @@
 import dayjs from "dayjs";
 
-import { DUMMY_CONTACTS, NOTE_TAGS } from "../constants";
+import { DUMMY_CONTACTS } from "../constants";
 
 export const calculateElapsedTime = timestamp =>
   dayjs.unix(timestamp).fromNow();
 
 export const formatTimeForTooltip = timestamp =>
   dayjs.unix(timestamp).format("dddd, h:mA");
-
-export const noteFormTagOptions = () =>
-  NOTE_TAGS.map(tag => ({ label: tag, value: tag }));
 
 export const noteFormContactOptions = () =>
   DUMMY_CONTACTS.map(contact => ({
